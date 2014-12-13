4aed7b3aa318c0da4a51bca70ecd26c2ef832173
ZHP <zhpzhp111@qq.com>
2014-12-13 14:44:33 +0800

    修改程序图标和名称。

--- app/src/main/java/com/example/zhp/coolweather/util/Utility.java
+++ app/src/main/java/com/example/zhp/coolweather/util/Utility.java
@@ -131,7 +131,7 @@ public class Utility {
 		editor.putString("temp1", temp1);
 		editor.putString("temp2", temp2);
 		editor.putString("weather_desp", weatherDesp);
-		editor.putString("publishTime", publishTime);
+		editor.putString("publish_time", publishTime);
 		editor.putString("current_date", sdf.format(new Date()));
 		editor.commit();
 	}
